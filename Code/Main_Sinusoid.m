6d870a1bde408cf36bf43086fdfd741d1c5d6524
70ViRNaND <50825680+70ViRNaND@users.noreply.github.com>
2020-04-03 16:03:41 +0700

    Update Main_Sinusoid.m

--- Code/Main_Sinusoid.m
+++ Code/Main_Sinusoid.m
@@ -1,4 +1,4 @@
-%% Main Non-ECG Filtering Procedure
+%% Main Sinusoid Filtering Procedure
 
 %fs =30;
  % fs  =70;
@@ -25,22 +25,22 @@ order = length(y);
 cut_off_freq = 150/(fs/2);
 alpha = fs/(2*cut_off_freq);
 rect = cut_off_freq*sinc(cut_off_freq*(-(order/2):(order/2)));
-windowed =  rect ; % Jendela Persegi 
-%windowed =  gausswin(order+1,alpha)'; % Jendela Gauss
-%windowed =   rect.*gausswin(order+1,alpha/30)'; % Jendela Persegi yang diperhalus (eliminasi fenomena Gibbs)
+windowed =  rect ; % Rectangular Window 
+%windowed =  gausswin(order+1,alpha)'; % Gaussian Window ('alpha' arguments is tunable)
+%windowed =   rect.*gausswin(order+1,alpha/30)'; % Smooth Rectangular Window (Smooth mean Gibbs Phenomenon Eliminated)
 
 h =windowed;
 
 fh = (fftshift(fft(h,nfft2)));
 fhfshift = (-nfft2/2:nfft2/2-1)*(fs/nfft2);
-fhpowershift = abs(fh).^2/1; %Update Terbaru
+fhpowershift = abs(fh).^2/1; 
 
-mul = fypowershift.*fhpowershift;
-convo = conv(y,h);
+mul = fypowershift.*fhpowershift; %frequency domain operation
+convo = conv(y,h); %time domain operation
 
 fconvo = (fftshift(fft(convo,length(convo))));
 fconvofshift = (-length(convo)/2:length(convo)/2-1)*(fs/length(convo));
-fconvopowershift = abs(fconvo).^2/1;%length(convo)
+fconvopowershift = abs(fconvo).^2/1;
 
 invfy = (ifft(ifftshift(fy,length(fyfshift))));
 invfh = (ifft(ifftshift(fh,length(fhfshift))));
@@ -49,57 +49,56 @@ invconvo = (ifft(ifftshift(fconvo,length(fconvofshift))));
 subplot(3,3,1)
 plot(dt,y)
 hold on 
-%plot((1:length(dt)),invfy(1:length(dt)),'red');
 plot(dt,invfy(1:length(dt)),'red')
-title('Sinyal Domain Waktu Pra Filtrasi')
-xlabel('Sampel Waktu')
-ylabel('Amplitudo')
-legend('original signal','sampled signal')
+title('Time Domain Pre-Filtered Sinusoid')
+xlabel('Time Sample')
+ylabel('Amplitude')
+legend('Original Signal','Sampled Signal')
 
 subplot(3,3,2)
 plot(fyfshift,fypowershift)
-title('Sinyal Domain Frekuensi Pra Filtrasi')
-xlabel('Frekuensi')
-ylabel('Magnitudo')
+title('Frequency Domain Pre-Filtered Sinusoid')
+xlabel('Frequency')
+ylabel('Magnitude')
 
  subplot(3,3,3)
  plot((1:length(dt)),invfy(1:length(dt)),'red');
-title('IDFT Sinyal Domain Frekuensi')
-xlabel('Sampel Waktu')
-ylabel('Amplitudo')
+title('IDFT of Frequency Domain Pre-Filtered Sinusoid')
+xlabel('Time Sample')
+ylabel('Amplitude')
 
  subplot(3,3,4)
  plot(h)
- title('Filter Domain Waktu') 
- xlabel('Sampel Waktu')
- ylabel('Respon')
+ title('Time Domain Filter/Window') 
+ xlabel('Time Sample')
+ ylabel('Response')
  
 subplot(3,3,5)
 plot(fhfshift,fhpowershift)
-title('Filter Domain Frekuensi')
-xlabel('Frekuensi')
-ylabel('Respon')
+title('Frequency Domain Filter/Window')
+xlabel('Frequency')
+ylabel('Response')
 
 subplot(3,3,6)
 plot((1:nfft2),abs(invfh(1:nfft2)),'red');
-title('IDFT Simple-Bounded Gaussian Window Domain Frekuensi')
-xlabel('Sampel Waktu')
-ylabel('Respon')
+title('IDFT of Frequency Domain Filter/Window'')
+xlabel('Time Sample')
+ylabel('Response')
 
 subplot(3,3,7)
 plot((convo))
-title('Sinyal Domain Waktu Pasca Filtrasi') 
-xlabel('Sampel Waktu')
-ylabel('Amplitudo')
+title('Time Domain Post-Filtered Sinusoid') 
+xlabel('Time Sample')
+ylabel('Amplitude')
     
 subplot(3,3,8)
 plot(fconvofshift,fconvopowershift)
-title('Sinyal Domain Frekuensi Pasca Filtrasi') 
-xlabel('Sampel Waktu')
-ylabel('Magnitudo')
+title('Frequency Domain Post-Filtered Sinusoid') 
+xlabel('Frequency')
+ylabel('Magnitude')
 
 subplot(3,3,9)
 plot(-invconvo,'red')
-title('IDFT Sinyal Domain Waktu Pasca Filtrasi') 
-xlabel('Sampel Waktu')
-ylabel('Amplitudo')
+title('IDFT of Frequency Domain Post-Filtered Sinusoid') 
+xlabel('Time Sample')
+ylabel('Amplitude')
